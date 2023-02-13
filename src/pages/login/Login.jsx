8976335a1ed957a6e90c313ff2ce3436c47733c4
Lucas Lima <developerlucaslima@gmail.com>
2023-02-13 15:00:06 -0300

    first commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,29 @@
+import { Container } from "./styles"
+
+export function Login(){
+  return(
+    <Container>
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </Container>
+  )
+
+}
