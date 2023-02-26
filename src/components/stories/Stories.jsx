884a1819b2cb292a35e8baab87d746da99c66197
Lucas Lima <developerlucaslima@gmail.com>
2023-02-26 20:55:54 -0300

    authcontext error

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -1,9 +1,49 @@
 import { Container } from "./styles"
 
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
 export function Stories(){
-  return(
+  const {currentUser} = useContext(AuthContext)
+
+  //TEMPORARY
+  const stories = [
+    {
+      id: 1,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 3,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 4,
+      name: "John Doe",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
+
+  return (
     <Container>
-      <h1>Stories</h1>
+      <div className="story">
+          <img src={currentUser.profilePic} alt="" />
+          <span>{currentUser.name}</span>
+          <button>+</button>
+        </div>
+      {stories.map(story=>(
+        <div className="story" key={story.id}>
+          <img src={story.img} alt="" />
+          <span>{story.name}</span>
+        </div>
+      ))}
     </Container>
   )
 }
+
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,11 +1,10 @@
-import { createContext, useEffect, useState } from "react";
+import { useEffect } from "react"
+import { useState } from "react"
+import { createContext } from "react"
 
-export const AuthContext = createContext();
-
+export const AuthContext = createContext()
 export const AuthContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user")) || null
-  );
+  const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null)
 
   const login = () => {
     //TO DO
